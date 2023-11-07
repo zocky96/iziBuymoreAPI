153f0f5cb153b1c17b976c814bd9c712caae4439
zocky96 <zocky58@gmail.com>
2023-11-07 04:11:01 +0100

    first commit

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 //import Product from "../models/product.js"
 //import Images from "../models/SlideImages.js"
-//let seq = new Sequelize("sysizi","root","arnob123",{dialect:'mysql',host: '198.71.48.131'})
-let seq = new Sequelize("izi","izi","1234",{dialect:'mysql',host: 'localhost'})
+let seq = new Sequelize("sysizi","root","arnob123",{dialect:'mysql',host: '198.71.48.131'})
+//let seq = new Sequelize("izi","izi","1234",{dialect:'mysql',host: 'localhost'})
 //import Product from "../models/product.js" 
 //Product.hasMany(Images)
 //Images.belongsTo(Product)
